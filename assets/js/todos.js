7a1ba6511762149c1e5ec5d5fcbcdc049f3c623a
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2019-08-01 13:40:33 -0500

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,10 @@
+//check off specific todos by clicking
+
+$('li').click(function(){
+    $(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$('span').click(function(){
+    
+});
